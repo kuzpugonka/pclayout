2b67a492a6103c1eb45c0bc9285f589349c93ff3
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2024-05-31 15:01:39 +0300

    add modal

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+const modalBtn = document.querySelector(".modal__button");
+const modal = document.querySelector(".modal");
+
+modalBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+modal.addEventListener("click", (e) => {
+  const modalContent = e.target.closest(".modal__inner");
+  if (!modalContent) {
+    modal.style.display = "";
+  }
+});
