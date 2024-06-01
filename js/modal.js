93e3102aba1275c1c44ff96d1f0bcd8ffeb61492
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2024-06-01 15:01:25 +0300

    add course-open-modal

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 const modal = () => {
   const modalBtn = document.querySelector(".modal__button");
+  const courseBtn = document.querySelector('.course__button');
   const modal = document.querySelector(".modal");
   const modalInner = document.querySelector(".modal__inner");
 
@@ -29,6 +30,10 @@ const modal = () => {
   };
 
   modalBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+  });
+  
+  courseBtn.addEventListener("click", () => {
     modal.style.display = "flex";
   });
 
